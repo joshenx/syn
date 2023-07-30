04d7b1683b9383dfb365bcfc8a28836d65f97494
joshenx <e0525998@u.nus.edu>
2023-07-30 19:19:32 +0800

    add tanstack query

--- src/App/components/supabaseClient.tsx
+++ src/App/components/supabaseClient.tsx
@@ -4,37 +4,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)
-
-// export const DisplayError = () => {
-//   const { isOpen, onOpen, onClose } = useDisclosure();
-//   const cancelRef = useRef(null);
-
-//   return (
-//     <AlertDialog
-//         isOpen={isOpen}
-//         leastDestructiveRef={cancelRef}
-//         onClose={onClose}
-//       >
-//         <AlertDialogOverlay>
-//           <AlertDialogContent>
-//             <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-//               Delete Customer
-//             </AlertDialogHeader>
-
-//             <AlertDialogBody>
-//               Are you sure? You can't undo this action afterwards.
-//             </AlertDialogBody>
-
-//             <AlertDialogFooter>
-//               <Button ref={cancelRef} onClick={onClose}>
-//                 Cancel
-//               </Button>
-//               <Button colorScheme='red' onClick={onClose} ml={3}>
-//                 Delete
-//               </Button>
-//             </AlertDialogFooter>
-//           </AlertDialogContent>
-//         </AlertDialogOverlay>
-//       </AlertDialog>
-//   )
-// }
